f9ba58cdf88e91521a0e2207751496d91f3d0041
vitinkkjk . <vitorvillelath@gmail.com>
2026-04-01 20:38:41 -0300

    Primeiro commit

---
+++ ClientManager/src/main/Main.java
@@ -0,0 +1,9 @@
+package src.main;
+import src.view.UserView;
+
+public class Main {
+    public static void main(String[] args) {
+        UserView userView = new UserView();
+        userView.loginScreen();
+    }
+}
