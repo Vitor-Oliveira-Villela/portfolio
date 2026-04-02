3f8a966ef44ef77772ecf29e3c4f35d473c54046
Vitin.Villela <vitorvillelath@gmail.com>
2026-04-01 21:20:40 -0300

    Delete ClientManager directory

--- ClientManager/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package src.main;
-import src.view.UserView;
-
-public class Main {
-    public static void main(String[] args) {
-        UserView userView = new UserView();
-        userView.loginScreen();
-    }
-}
