253c2884675c4cbe660a539141ebcdc1511523c9
vitinkkjk . <vitorvillelath@gmail.com>
2026-04-01 21:21:41 -0300

    Primeiro Commit

---
+++ ClientManager/src/main/java/com/clientmanager/Main.java
@@ -0,0 +1,9 @@
+package src.main.java.main;
+import view.UserView;
+
+public class Main {
+    public static void main(String[] args) {
+        UserView userView = new UserView();
+        userView.loginScreen();
+    }
+}
---
+++ ClientManager/src/main/java/com/clientmanager/view/UserView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.util.Scanner;
+
+public class UserView {
+
+    public void loginScreen() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("------Tela de Login------");
+        System.out.println("Insira o nome de usuario ou email cadastrado:");
+        String username = sc.nextLine();
+
+        System.out.println("Insira a senha:");
+        String password = sc.nextLine();
+
+        
+
+        //if () {
+            System.out.println("Nome:");
+            String nome = sc.next();
+       //}
+    }
+}
